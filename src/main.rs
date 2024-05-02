f9b408662de4a0470c060f2550c49216789357a8
magisystem0408 <61937077+magisystem0408@users.noreply.github.com>
2024-05-02 17:12:00 +0900

    feat: first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::HttpServer;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .service(actix_web::web::scope("/v1")
+                .route("/profile", actix_web::web::get().to(index)))
+            .route("/profile", actix_web::web::post().to(index))
+    })
+        .bind(("127.0.0.1", 8001))?
+        .run()
+        .await
+}
+
+async fn index() -> &'static str {
+    "Hello world!"
+}
