63142df8bffd3e78cf5150531f5ca12defe3812f
magisystem0408 <masato.matsudo@gmail.com>
2024-05-05 19:37:15 +0900

    feat: add global state

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use actix_web::HttpServer;
+use actix_web::{App, HttpServer};
+
+#[derive(Clone)]
+struct Messenger {
+    message: String,
+}
+
+#[derive(Clone)]
+struct AppState {
+    messenger: Messenger,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        actix_web::App::new()
-            .service(
-                actix_web::web::scope("/v1").route("/profile", actix_web::web::get().to(index)),
-            )
-            .route("/profile", actix_web::web::post().to(index))
+    let app_state = AppState {
+        messenger: Messenger {
+            message: "hello".to_string(),
+        },
+    };
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(actix_web::web::Data::new(app_state.clone()))
+            .route("/", actix_web::web::post().to(update))
+            .route("/", actix_web::web::get().to(get))
     })
-        .bind(("127.0.0.1", 8001))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8001))?
+    .run()
+    .await
 }
 
-async fn index() -> &'static str {
-    "Hello world!"
+async fn update(app_data: actix_web::web::Data<AppState>) -> String {
+    let messanger = app_data.messenger.clone();
+    format!("{} world", messanger.message)
+}
+
+async fn get(app_data: actix_web::web::Data<AppState>) -> String {
+    app_data.messenger.message.clone()
 }
